lightningdevkit/orange-sdk

commit 69defd3ba0c8daedd9ceb61a129d3b42acbcef7a

Rebalance after `Event`s rather than on a timer. (#32)

orange-sdk/src/lib.rs
@@ -607,15 +607,14 @@ impl Wallet {
 			Arc::clone(&logger),
 		));
 
-		// Spawn a background thread that every second, we see if we should initiate a rebalance
-		// This will withdraw from the trusted balance to our LN balance, possibly opening a channel.
+		// Spawn a background thread to initiate a rebalance if needed.
+		// We only do this once as we generally rebalance in response to
+		// `Event`s which indicated our balance has changed.
 		let rb = Arc::clone(&rebalancer);
 		runtime.spawn_cancellable_background_task(async move {
-			loop {
-				rb.do_rebalance_if_needed().await;
-
-				tokio::time::sleep(Duration::from_secs(1)).await;
-			}
+			// Wait a second to get caught up, then try to rebalance.
+			tokio::time::sleep(Duration::from_secs(1)).await;
+			rb.do_rebalance_if_needed().await;
 		});
 
 		let inner = Arc::new(WalletImpl {
@@ -1333,12 +1332,20 @@ impl Wallet {
 	///
 	/// **Note:** This **MUST** be called after each event has been handled.
 	pub fn event_handled(&self) -> Result<(), ()> {
-		self.inner.runtime.block_on(self.inner.event_queue.event_handled()).map_err(|e| {
+		let res = self.inner.runtime.block_on(self.inner.event_queue.event_handled()).map_err(|e| {
 			log_error!(
 				self.inner.logger,
 				"Couldn't mark event handled due to persistence failure: {e}"
 			);
-		})
+		});
+		if res.is_ok() {
+			// If an event was handled, probably our balances changed and we may need to rebalance.
+			let inner_ref = Arc::clone(&self.inner);
+			self.inner.runtime.spawn_cancellable_background_task(async move {
+				inner_ref.rebalancer.do_rebalance_if_needed().await;
+			});
+		}
+		res
 	}
 
 	/// Stops the wallet, which will stop the underlying LDK node and any background tasks.

commit 0474476bf2f374f55a7eb3ed01f3097408591ff3

Fix onchain rebalancing
69defd3ba0c8daedd9ceb61a129d3b42acbcef7a broke on-chain rebalancing by
making it so we only do rebalances based on events. This works for
trusted rebalances because we actually receive events. Sadly for
ldk-node we don't receive on-chain events yet so we need to use our own
rolled version of it, for this to work we need the loop.

graduated-rebalancer/src/lib.rs
@@ -248,10 +248,20 @@ where
 
 	/// Does any rebalance if it meets the conditions of the tunables
 	pub async fn do_rebalance_if_needed(&self) {
+		self.do_trusted_rebalance_if_needed().await;
+
+		self.do_onchain_rebalance_if_needed().await;
+	}
+
+	/// Does a trusted to lightning rebalance if needed
+	pub async fn do_trusted_rebalance_if_needed(&self) {
 		if let Some(params) = self.trigger.needs_trusted_rebalance().await {
 			self.do_trusted_rebalance(params).await;
 		}
+	}
 
+	/// Does an on-chain to lightning rebalance if needed
+	pub async fn do_onchain_rebalance_if_needed(&self) {
 		if let Some(params) = self.trigger.needs_onchain_rebalance().await {
 			self.do_onchain_rebalance(params).await;
 		}

orange-sdk/src/lib.rs
@@ -615,6 +615,15 @@ impl Wallet {
 			// Wait a second to get caught up, then try to rebalance.
 			tokio::time::sleep(Duration::from_secs(1)).await;
 			rb.do_rebalance_if_needed().await;
+
+			// create loop for onchain rebalancing.
+			// we only do onchain rebalancing here as trusted rebalancing is
+			// handled by events but we cannot do that for onchain rebalancing.
+			// So we need to detect onchain balance changes periodically.
+			loop {
+				tokio::time::sleep(Duration::from_secs(1)).await;
+				rb.do_onchain_rebalance_if_needed().await;
+			}
 		});
 
 		let inner = Arc::new(WalletImpl {

orange-sdk/tests/integration_tests.rs
@@ -392,6 +392,7 @@ async fn test_receive_onchain() {
 			.unwrap();
 
 		wait_for_tx(&electrsd.client, sent_txid).await;
+		println!("payment sent with txid: {sent_txid}");
 
 		// confirm transaction
 		generate_blocks(&bitcoind, &electrsd, 6).await;
@@ -404,8 +405,8 @@ async fn test_receive_onchain() {
 		})
 		.await;
 
+		println!("waiting for onchain recv event");
 		let event = wait_next_event(&wallet).await;
-
 		match event {
 			Event::OnchainPaymentReceived { txid, amount_sat, status, .. } => {
 				assert_eq!(txid, sent_txid);
@@ -445,6 +446,7 @@ async fn test_receive_onchain() {
 		tokio::time::sleep(Duration::from_secs(5)).await; // wait for sync
 
 		//  wait for rebalance to be initiated
+		println!("waiting for channel opened event");
 		let event = wait_next_event(&wallet).await;
 		match event {
 			Event::ChannelOpened { counterparty_node_id, .. } => {